b8b70e32d0f3203004807c2b29ef11596e1f46a0
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-28 21:50:32 +0100

    feat: add base template and static files
    Base template wil still be modified and not all static sources have been properly linked within it (mainly the image links, but it's okay, since those won't be used directly, their urls will be obtained from the database).

--- catshef/catshef/settings.py
+++ catshef/catshef/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE_CLASSES = [
