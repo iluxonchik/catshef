a40104e3b8dd8c7dee8495ae1a90202530a8094a
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-07-29 12:53:14 +0100

    feat: add Product, Category and ProductImage models and update test_models.py
    All of the unit tests are passing now.

--- catshef/products/models.py
+++ catshef/products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField()
+    description = models.TextField()
+    parent = models.ForeignKey('Category', null=True, 
+        related_name='child_categories', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField()
+    description = models.TextField()
+    categories = models.ManyToManyField('Category', related_name='products')
+    stock = models.PositiveSmallIntegerField()  # range: [0, 32767]
+    # TODO: set default image (replace null=True) or just use default image
+    # in template rendering (decide)
+    main_image = models.ForeignKey('ProductImage', 
+        related_name='main_image_of', null=True, on_delete=models.SET_NULL)
+    available = models.BooleanField(default=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    # image will be uploaded to MEDIA_ROOT/products/%Y/%m/%d/
+    image = models.ImageField(upload_to='products/%Y/%m/%d/')
+    product = models.ForeignKey('Product', related_name='images')
+    # TODO: Order field
+    
--- catshef/products/tests/test_models.py
+++ catshef/products/tests/test_models.py
@@ -1,26 +1,29 @@
+import pdb
+
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from products.models import Product, Category, ProductImage
 
-
 class ProductsModelTestCase(TestCase):
     
     def setUp(self):
-        image_path = 'catshef/products/tests/resources/img/chicken_breast.jpg'
+        image_path = 'products/tests/resources/img/chicken_breast.jpg'
         
         self.cat1 = Category.objects.create(
             name='meat', 
-            slug='meat', 
+            slug='meat',
+            description='The meat category.', 
             parent=None)
 
         self.product1 = Product.objects.create(
-            category=self.cat1,
             name='Chicken Breast',
             slug='chicken-breast',
-            description='Chicken breast. Yes, chicken breast',
+            description='Chicken breast. Yes, chicken breast.',
             stock=120,
             available=True)
+
+        self.product1.categories.add(self.cat1)
 
         self.prod_img = ProductImage.objects.create(image=SimpleUploadedFile(
                 name='product1_img.jpg',
@@ -39,13 +42,30 @@ class ProductsModelTestCase(TestCase):
         """
         self.assertEqual(self.cat1.name, 'meat')
         self.assertEqual(self.cat1.slug, 'meat')
+        self.assertEqual(self.cat1.description, 'The meat category.')
         self.assertIsNone(self.cat1.parent)
 
-        self.assertEqual(self.product1.category, self.cat1)
+        self.assertEqual(len(self.product1.categories.all()), 1)
+        self.assertEqual(self.product1.categories.all()[0], self.cat1)
         self.assertEqual(self.product1.name, 'Chicken Breast')
         self.assertEqual(self.product1.slug, 'chicken-breast')
-        self.assertEqual('product1_img.jpg', self.product1.main_image.name)
+        self.assertIn('product1_img', self.product1.main_image.image.name)
         self.assertEqual(self.product1.description, 
-            'Chicken breast. Yes, chicken breast')
+            'Chicken breast. Yes, chicken breast.')
         self.assertEqual(self.product1.stock, 120)
         self.assertTrue(self.product1.available)
+        
+        # Test adding multiple categories to a product        
+        high_protein = self.product1.categories.create(name='high protein',
+                                        slug='high-protein',
+                                        description='Hight protein foods.',
+                                        parent=self.cat1)
+
+        self.assertEqual(len(self.product1.categories.all()), 2)
+        self.assertIn(high_protein, self.product1.categories.all())
+        self.assertEqual(high_protein.parent, self.cat1)
+
+
+        # Test __str()__ returns expected name
+        self.assertEqual(str(self.product1), 'Chicken Breast')
+        self.assertEqual(str(self.cat1), 'meat')
