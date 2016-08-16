240d05f661844a63b1778706db268958c52201f1
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-08-16 17:48:09 +0100

    refactor: add_class and add_placeholder now return BoundField, generalize add_<attr_name> methods.
    `as_widget()` returned the HTML representation, now it returns what it recieves: a BoundField.
    This allows for some "filter chaining" (with the "with" tag).

--- catshef/account/templatetags/html_filters.py
+++ catshef/account/templatetags/html_filters.py
@@ -2,6 +2,43 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='add_attrs')
-def add_attrs(value, arg):
-    return value.as_widget(attrs={'class':arg})
+def _add_attr_to_boundfield(bound_field, attr_name, attr_value,
+    override=False, separator=' '):
+    """
+    Generic method for adding attributes to bound fields.
+
+    Args:
+        bound_fild (BoundField): BoundField instance to add the attr to
+        attr_name (str): name of new attribute to be added
+        attr_value (str): value of the new attribute
+        override (bool): indicates wether attr_name should be overriden(i.e.
+            replaced), if it's already in the Fields widget attributes
+        separator (str): string used to append the attr_value if attr_name is 
+            already in the Field widget attributes (only has effect if  
+            override=False)
+    """
+    field = bound_field.field  # related Form field instance
+    field_attrs = field.widget.attrs  # attrs of current widget
+
+    if override:
+        field.widget.attrs = {**field_attrs, **{attr_name: attr_value}}
+        return bound_field
+    # Don't override, append the content if the attr_name being added is already
+    # present    
+    if attr_name in field_attrs:
+        # if an attribute that's being added is already present, then append
+        # the contents of the new attribute to it
+        field_attrs[attr_name] += separator + attr_value
+    else:
+        # if the attribute being added is a new one, then simply merge both of
+        # of the dicts
+        field.widget.attrs = {**field_attrs, **{attr_name: attr_value}}
+    return bound_field
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    return _add_attr_to_boundfield(value, 'class', arg)
+
+@register.filter(name='add_placeholder')
+def add_placeholder(value, arg):
+    return _add_attr_to_boundfield(value, 'placeholder', arg, override=True)
--- catshef/account/test/test_filters.py
+++ catshef/account/test/test_filters.py
@@ -1,17 +1,66 @@
 from django import forms
+from django.forms import BoundField
 
 from django.test import SimpleTestCase
 
-from account.templatetags.filters import add_attrs
+from account.templatetags.html_filters import add_class, add_placeholder
 
 class TestForm(forms.Form):
     test = forms.CharField()
+    test2 = forms.CharField(widget=forms.widgets.TextInput(attrs={
+        'placeholder':'plh'
+        }))
 
-class AddCssTestCase(SimpleTestCase):
-    def test_css_added(self):
+class HTMLFiltersTestCase(SimpleTestCase):
+    def test_add_class(self):
         data = {'test':'This is a test string'}
         f = TestForm(data=data)
         self.assertNotIn('class="some class"', str(f['test']))
-        result = add_attrs(f['test'], 'some class')
+        result = add_class(f['test'], 'some class')
+        self.assertTrue(type(result) is BoundField, 'Returned field has ' 
+            'wrong type')
         self.assertIn('class="some class"', str(result), 'Class not added to form '
             'field.')
+    
+    def test_add_placeholder(self):
+        data = {'test':'This is a test string'}
+        f = TestForm(data=data)
+        self.assertNotIn('placeholder="Test"', str(f['test']))
+        result = add_placeholder(f['test'], 'Test')
+        self.assertTrue(type(result) is BoundField, ('Returned field has ' 
+            'wrong type'))
+        self.assertIn('placeholder="Test"', str(result), 'Placeholder not added'
+            ' to form field.')
+
+    def test_original_attr_preserved(self):
+        """
+        Test that the existing field attrs are preserved.
+        """
+        data = {'test':'This is a test string', 'test2':''}
+        f = TestForm(data=data)
+        self.assertIn('placeholder="plh"', str(f['test2']))
+        self.assertNotIn('class="much class"', str(f['test2']))
+        result = add_class(f['test2'], 'much class')
+        self.assertTrue(type(result) is BoundField, ('Returned field has ' 
+            'wrong type'))
+        self.assertIn('placeholder="plh"', str(result), 'Exisitng attrs not'
+            ' preserved.')
+        self.assertIn('class="much class"', str(result), 'Class not added'
+            ' to form field.')
+
+    def test_placeholder_attr_replaced(self):
+        """
+        Test that an exisitng placeholder is replaced.
+        """
+        data = {'test':'This is a test string', 'test2':''}
+        f = TestForm(data=data)
+        self.assertIn('placeholder="plh"', str(f['test2']))
+        result = add_placeholder(f['test2'], 'new placeholder')
+        self.assertTrue(type(result) is BoundField, ('Returned field has ' 
+            'wrong type'))
+        self.assertNotIn('plh', str(result), 'Exisitng attrs'
+            ' preserved.')
+        self.assertIn('placeholder="new placeholder"', str(result), 'New '
+            'placeholder not added to form field.')
+
+
