5724cdda0ba6f32531fb7a27017680de25823b5f
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-08-24 17:13:25 +0100

    feat: add account urls
    test_urls.py now passes

--- catshef/account/urls.py
+++ catshef/account/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from account import views
 
 urlpatterns = [
+    url(r'^profile/$', views.edit_profile, name='edit_profile'),
     url(r'^$', views.profile, name='profile'),
     url(r'^', include('allauth.urls')), 
 ]
