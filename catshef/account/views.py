8b69a28f532351d29e4e33b56d0b40462c285f16
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-08-25 17:25:06 +0100

    feat: add email settings template

--- catshef/account/views.py
+++ catshef/account/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def profile(request):
+    return HttpResponse('')
+
+@login_required
+def edit_profile(request):
     return HttpResponse('')
