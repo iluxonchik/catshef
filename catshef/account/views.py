64a6068bc0bbd86854052d579532bf4b1b59467b
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-08-27 02:18:56 +0100

    feat: add Profile edit form
    closes #75 closes #53

--- catshef/account/views.py
+++ catshef/account/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from account.models import Profile
+from account.forms import ProfileEditForm
 
 @login_required
 def profile(request):
@@ -8,4 +12,16 @@ def profile(request):
 
 @login_required
 def edit_profile(request):
-    return HttpResponse('')
+    profile = request.user.profile
+    if request.method == 'POST':
+        profile_form = ProfileEditForm(request.POST, instance=profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Profile successfully updated')
+            return redirect(reverse('profile'))
+    else:
+        profile_form = ProfileEditForm(instance=profile)
+    return render(request, 'account/profile/edit_profile.html', 
+        {'profile_form': profile_form})
+
+
