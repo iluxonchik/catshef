a531ba161f55ac26fb2151be92413790c1dbd7a2
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-09-05 22:43:12 +0100

    feat: add to_decimal() util function
    additons to #94

--- catshef/products/utils/conversion.py
+++ catshef/products/utils/conversion.py
@@ -4,6 +4,8 @@ products utility functions
 from decimal import Decimal, ROUND_HALF_UP
 from catshef.exceptions import ArgumentError
 
+import collections
+
 def round_decimal(value, precision=2, rounding=ROUND_HALF_UP):
     """
     Round a Decimal to the given precision.
@@ -14,3 +16,33 @@ def round_decimal(value, precision=2, rounding=ROUND_HALF_UP):
     return (value.quantize(Decimal(10) ** -precision, rounding=rounding) 
         if value is not None 
         else value)
+
+def to_decimal(val, rounding=round_decimal):
+    """
+    Convers a value or list of values to Decimal.
+
+    Arguments:
+        * val - value(s) to convert
+        * rounding - callable wich takes a Decimal as an argument and returns
+          a Decimal. Applied to every value in val.
+    """
+    if isinstance(val, str):
+        val = (val, )
+
+    if not isinstance(val, collections.Iterable):
+        val = (val, )
+
+    ret_generator = True
+    if len(val) == 1:
+        ret_generator = False
+
+    if rounding:
+        res = (rounding(Decimal(value)) for value in val)
+    else:
+        res= (Decimal(value) for value in val)
+    
+    if not ret_generator:
+        res = next(res)
+    
+    return res
+
